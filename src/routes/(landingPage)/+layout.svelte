7b974ed7ed8b16ff1ade39e046946fa4e8d40fe7
Magnus Farstad <magnus.r.farstad@outlook.com>
2023-09-06 19:03:01 +0200

    Updated href link

--- src/routes/(landingPage)/+layout.svelte
+++ src/routes/(landingPage)/+layout.svelte
@@ -11,7 +11,7 @@
 		<div class="nav-elements">
 			<a href="/" class="nav-element">Home</a>
 			<a href="/" class="nav-element">Projects</a>
-			<a href="/details" class="nav-element">About</a>
+			<a href="/" class="nav-element">About</a>
 		</div>
 	</nav>
 </header>
